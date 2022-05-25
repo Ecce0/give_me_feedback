0d40ccc63565c58800f1342a46aeeaba5e4fd0b8
E. Collier <ecollier871@outlook.com>
2022-05-25 17:49:20 -0400

    whoops!

--- src/components/SingleFeedback.js
+++ src/components/SingleFeedback.js
@@ -1,31 +1,22 @@
-import React from "react"
+import React from 'react'
 import Card from './shared/Card'
 import PropTypes from 'prop-types'
 import { FaTimes } from 'react-icons/fa'
 
-
 const SingleFeedback = ({ statement, handleDelete }) => {
-
-   
-  return (
-    <Card reverse={true}>
-      <div className="num-display">
-        {statement.rating}
-      </div>
-      <button 
-       className="close"
-       onClick={handleDelete}
-       >
-        <FaTimes color='orange' />
-      </button>
-      <div className="text-display">{statement.text}</div>      
-    </Card>
-  )
-
+	return (
+		<Card reverse={true}>
+			<div className='num-display'>{statement.rating}</div>
+			<button className='close' onClick={() => handleDelete(statement.id)}>
+				<FaTimes color='orange' />
+			</button>
+			<div className='text-display'>{statement.text}</div>
+		</Card>
+	)
 }
 
 SingleFeedback.propTypes = {
-  statement: PropTypes.object.isRequired,
+	statement: PropTypes.object.isRequired,
 }
 
 export default SingleFeedback

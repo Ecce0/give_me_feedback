4826cb66d967c360be41a88ece6f760795e47c92
E. Collier <ecollier871@outlook.com>
2022-07-19 10:44:12 -0400

    build working

--- src/components/context/FeedbackContext.js
+++ src/components/context/FeedbackContext.js
@@ -1,27 +1,11 @@
 import React, { createContext, useState } from 'react'
 import { v4 as uuidv4} from "uuid"
-
+import initialData from '../data/FeedbackData'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-	const [feedback, setFeedback] = useState([
-		{
-			id: 1,
-			text: 'Feedback item 1',
-			rating: 10,
-		},
-		{
-			id: 2,
-			text: 'Feedback item 2',
-			rating: 9,
-		},
-		{
-			id: 3,
-			text: 'Feedback item 3',
-			rating: 7,
-		},
-	])
+	const [feedback, setFeedback] = useState(initialData)
 
 	const [edit, setEdit] = useState({
 		item: {},
